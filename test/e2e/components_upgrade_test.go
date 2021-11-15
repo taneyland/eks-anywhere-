f7e6bdb0bcfe44a226635beb4215d9096164169d
Taylor Neyland <tneyla@amazon.com>
2021-11-15 16:31:00 -0600

    add code to download and unpack tarballs

--- test/e2e/components_upgrade_test.go
+++ test/e2e/components_upgrade_test.go
@@ -3,13 +3,26 @@
 package e2e
 
 import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/aws/eks-anywhere/pkg/cluster"
 	"github.com/aws/eks-anywhere/pkg/semver"
+	"github.com/aws/eks-anywhere/pkg/validations"
 	releasev1alpha1 "github.com/aws/eks-anywhere/release/api/v1alpha1"
 	"github.com/aws/eks-anywhere/test/framework"
 )
 
-const prodReleasesManifest = "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml"
+const (
+	prodReleasesManifest = "https://anywhere-assets.eks.amazonaws.com/releases/eks-a/manifest.yaml"
+	releaseBinaryName    = "eksctl-anywhere"
+)
 
 func getLatestMinorReleaseFromMain(test *framework.ClusterE2ETest) *releasev1alpha1.EksARelease {
 	reader := cluster.NewManifestReader()
@@ -67,4 +80,72 @@ func getLatestMinorReleaseFromReleaseBranch(test *framework.ClusterE2ETest, rele
 	}
 
 	return latestPrevMinorRelease
+}
+
+func getBinary(test *framework.ClusterE2ETest, release releasev1alpha1.EksARelease) string {
+	latestReleaseBinaryFolder := filepath.Join("bin", release.Version)
+	latestReleaseBinaryPath := filepath.Join(latestReleaseBinaryFolder, releaseBinaryName)
+
+	if !validations.FileExists(latestReleaseBinaryPath) {
+		test.T.Logf("Reading prod latest release tarball %s", release.EksABinary.LinuxBinary.URI)
+		reader := cluster.NewManifestReader()
+		latestReleaseTar, err := reader.ReadFile(release.EksABinary.LinuxBinary.URI)
+		if err != nil {
+			test.T.Fatalf("Failed downloading tar for latest release: %s", err)
+		}
+
+		test.T.Log("Unpacking the release tarball")
+
+		err = unpackTarball(latestReleaseBinaryFolder, bytes.NewReader(latestReleaseTar))
+		if err != nil {
+			test.T.Fatalf("Failed unpacking artifacts for latest release: %s", err)
+		}
+	}
+
+	return latestReleaseBinaryPath
+}
+
+func unpackTarball(destinationFolder string, r io.Reader) error {
+	gzr, err := gzip.NewReader(r)
+	if err != nil {
+		return err
+	}
+	defer gzr.Close()
+
+	tr := tar.NewReader(gzr)
+
+	err = os.MkdirAll(destinationFolder, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	for {
+		header, err := tr.Next()
+		if err == io.EOF {
+			return fmt.Errorf("Binary [%s] not found in tarball", releaseBinaryName)
+		}
+		if err != nil {
+			return err
+		}
+		if header != nil && strings.TrimPrefix(header.Name, "./") == releaseBinaryName {
+			break
+		}
+
+		target := filepath.Join(destinationFolder, header.Name)
+		if header.Typeflag != tar.TypeReg {
+			return fmt.Errorf("Invalid type flag [%b] for binary [%s]", header.Typeflag, releaseBinaryName)
+		}
+
+		f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+		if err != nil {
+			return err
+		}
+
+		if _, err := io.Copy(f, tr); err != nil {
+			return err
+		}
+
+		f.Close()
+	}
+	return nil
 }
